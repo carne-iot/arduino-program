425ea282dfa5f546034f23b1f196ff3c8b2fccc3
natalia navas <natinavas@gmail.com>
2017-11-22 22:53:42 -0300

    sending correct temperature works

--- arduino/arduino.ino/arduino.ino.ino/arduino.ino.ino.ino
+++ arduino/arduino.ino/arduino.ino.ino/arduino.ino.ino.ino
@@ -1,44 +1,44 @@
-#include <Arduino.h>
+
+
+//Sensor libraries
+#include <Wire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+//Http communication libraries
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-#include <OneWire.h>
+ //uncomment  lines 41 to 57 if you get errors about pin definations (eg. error: unknown type name 'D1' etc) while compiling. This is to map exact GPIOs of ESP8266 with NodeMCU pins
+// refer following link for more details - https://github.com/esp8266/Arduino/blob/master/variants/nodemcu/pins_arduino.h#L37-L59 
+// if ESP8266 package for Arduino is updated to latest version 2.1.0 or above, then pins_arduino.h should be automatically downloaded and errors for unknown type name should be solved
 
-#include <DallasTemperature.h>
 
-#define LED_BUILTIN 2
+#define ONE_WIRE_BUS D1
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);   // constructor for our DS18B20 sensor to read temperature using DallasTemperature libary
 
 //wifi credentials
-char* ssid = "vloreto2";
+char* ssid = "vloteto";
 char* pass = "0070071234";
 //device credentials
 const String token = "eyJhbGciOiJIUzUxMiJ9.eyJ1aWQiOjAsImp0aSI6ODU0ODA2ODA2MjQ2MTkyNTcxNywicm9sZXMiOlsiUk9MRV9ERVZJQ0UiXSwiZGlkIjoxMjM0MTIzNCwic3ViIjoidXNlcm5hbWUiLCJpYXQiOjE1MTEzMDkzNzIsImV4cCI6OTIyMzM3MjAzNjg1NDc3NX0.9ZF-JR1ehWkjJVmBFGgMxODLl1h_KGXlC6Kh4ov3Ppb_39a8wi3JFtXKf7Pbg6NOzcnFzd5s6oY-cMUhAWylDA";
 
-//TODO setear bien el pin que se use
-const int sensorPin = 6;
 
-OneWire ds(sensorPin);
-DallasTemperature sensor(&ds);
-
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup()   {                
   Serial.begin(9600);
+  sensors.begin();
   connectWifi();
-  sensor.begin();
-}
+  }
 
 void loop() {
 
-   //Turn led on
-  digitalWrite(LED_BUILTIN,LOW);
-  delay(1000);
-  
-  if(WiFi.status() == WL_CONNECTED){
+
+
+if(WiFi.status() == WL_CONNECTED){
 
     //Read Temperature
-    sensor.requestTemperatures();
-    float temperature = sensor.getTempCByIndex(0);
-    
+    float temperature = getTemp();
     Serial.print("temperature: ");
     Serial.println(temperature); 
 
@@ -61,11 +61,18 @@ void loop() {
     Serial.println("Wifi not connected");
   }
 
-  //Turn led off
-  digitalWrite(LED_BUILTIN,HIGH);
-  delay(1000);
-
 }
+
+float getTemp(void) {   // function to read and display temperature
+  
+  sensors.requestTemperatures();           // Send the command to DS18B20 to get temperatures
+  float temperatureDegC = sensors.getTempCByIndex(0);
+  Serial.print("Temperature (Deg C):");   // enable just for debugging
+  Serial.println(temperatureDegC);  // enable just for debugging prints the temperature in deg C 
+ return temperatureDegC;
+ //delay(500);  // wait for 3 seconds and then clear display, you can adjust this time to increase or decrease duration of displaying Deg C or Deg F
+}
+
 
 void connectWifi(){
   WiFi.begin(ssid, pass);
@@ -80,48 +87,5 @@ void connectWifi(){
   
 }
 
-float getTemp(){
-  byte data[12];
-  byte addr[8];
 
-  if ( !ds.search(addr)){
-     
-     ds.reset_search();
-     return -1000;
-  }
-
-  if ( OneWire::crc8( addr, 7) != addr[7]) {
-    Serial.println("CRC no valido");
-    return -1000;
-  }
-
-  if ( addr[0] != 0x10 && addr[0] != 0x28) {
-    Serial.print("Dispositivo no reconocido");
-    return -1000;
-  }
- 
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44,1); // Se envía comando para conversión de temperatura
- 
-  byte present = ds.reset();
-  ds.select(addr);  
-  ds.write(0xBE); // Lectura de scratchpad
- 
- 
-  for (int i = 0; i < 9; i++) { // Almacenamos los 9 bytes del scratchpad
-    data[i] = ds.read();
-  }
- 
-  ds.reset_search();
- 
-  // Valores de temperatura almacenados en lo bytes 0 y 1
-  byte MS_Byte = data[1];
-  byte LS_Byte = data[0];
-  // Conversion de temperatura en grados celsius
-  float tempRead = ((MS_Byte << 8) | LS_Byte); 
-  float TemperatureSum = tempRead * 0.0625;
- 
-  return TemperatureSum;
-}
 
